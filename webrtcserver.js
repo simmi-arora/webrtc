c4bcf4b9d35da9b396450d777406bf85fcd064c7
altanai <tara181989@gmail.com>
2016-05-03 17:50:21 +0530

    interactivity and styles

--- webrtcserver.js
+++ webrtcserver.js
@@ -2,12 +2,28 @@ var fs = require('fs');
 var _static = require('node-static');
 var https = require('https');
 
+
+var property = require('./propertyWriter.js')(fs);
+/*property.writeEnv();*/
+var properties= JSON.parse(property.readEnv());
+
+var folderPath="";
+if(properties.enviornment=="production"){
+  folderPath='.client/prod';
+}else if(properties.enviornment=="test"){
+  folderPath='.client/tests';
+}else{
+  folderPath='.client/build';
+}
+
+
 var file = new _static.Server('./client/build', {
     cache: 3600,
     gzip: true,
     indexFile: "index.html"
 });
 
+console.log(folderPath , file);
 /*var options = {
   key: fs.readFileSync('/etc/apache2/ssl/villageexpert.key'),
   cert: fs.readFileSync('/etc/apache2/ssl/edac0f74577a2bdf.crt'),
@@ -25,83 +41,16 @@ var options = {
   rejectUnauthorized: false
 };
 
-
 var app = https.createServer(options, function(request, response){
         request.addListener('end', function () {
         file.serve(request, response);
     }).resume();     
 });
-app.listen(8084);
+app.listen(properties.httpsPort);
 
+var realtimecomm= require('./realtimecomm.js')(app, properties);
 
-var io = require('socket.io').listen(app, {
-    log: false,
-    origins: '*:*'
-});
-
-io.set('transports', [
-    'websocket'
-]);
-
-var channels = {};
-
-io.sockets.on('connection', function (socket) {
-
-    console.log("connection ");
-    var initiatorChannel = '';
-
-    if (!io.isConnected) {
-        io.isConnected = true;
-    }
-
-    socket.on('new-channel', function (data) {  
-        if (!channels[data.channel]) {
-            initiatorChannel = data.channel;
-        }
-        console.log("------------new channel------------- ");
-        channels[data.channel] = data.channel;     
-        onNewNamespace(data.channel, data.sender);
-    });
-
-    socket.on('presence', function (channel) {
-        console.log("presence ");
-        var isChannelPresent = !! channels[channel.channel];
-        socket.emit('presence', isChannelPresent);
-    });
-
-    socket.on('disconnect', function (channel) {
-    });
-
-});
-
-function onNewNamespace(channel, sender) {
-   console.log("onNewNamespace ");
-    io.of('/' + channel).on('connection', function (socket) {
-        
-        var username;
-        
-        if (io.isConnected) {
-            io.isConnected = false;
-            socket.emit('connect', true);
-        }
-
-        socket.on('message', function (data) {
-            console.log("message" , data);
-            if (data.sender == sender) {
-                if(!username) username = data.data.sender;                
-                socket.broadcast.emit('message', data.data);
-            }
-        });
-        
-        socket.on('disconnect', function() {
-            console.log("disconnect");
-            if(username) {
-                socket.broadcast.emit('user-left', username);
-                username = null;
-            }
-        });
-    });
-}
+console.log(" WebRTC server env => "+ properties.enviornment+ " running at\n "+properties.httpsPort+ "/\nCTRL + C to shutdown");
 
 
 /*var options = {
